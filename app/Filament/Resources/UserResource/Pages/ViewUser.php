2328fb59df781a6849478cf596d80a34bb50ad8a
Mahamoud BRAHIM ADOUM <moudefils@gmail.com>
2025-12-04 15:29:52 +0300

    Enhance user information display: add icons and placeholders for created and updated timestamps

--- app/Filament/Resources/UserResource/Pages/ViewUser.php
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -80,16 +80,24 @@ class ViewUser extends ViewRecord
                         Infolists\Components\Fieldset::make(__('ui.record_info'))
                             ->schema([
                                 Infolists\Components\TextEntry::make('createdBy.name')
-                                    ->label(__('ui.created_by')),
+                                    ->label(__('ui.created_by'))
+                                    ->icon('heroicon-o-user')
+                                    ->placeholder('-'),
                                 Infolists\Components\TextEntry::make('created_at')
                                     ->label(__('ui.created_at'))
+                                    ->icon('heroicon-o-calendar-days')
+                                    ->placeholder('-')
                                     ->dateTime(),
                                 Infolists\Components\TextEntry::make('updatedBy.name')
                                     ->visible(fn ($record) => filled($record->updated_by)) // Sadece güncelleyen varsa göster
-                                    ->label(__('ui.last_updated_by')),
+                                    ->label(__('ui.last_updated_by'))
+                                    ->icon('heroicon-o-user')
+                                    ->placeholder('-'),
                                 Infolists\Components\TextEntry::make('updated_at')
                                     ->visible(fn ($record) => filled($record->updated_by))
                                     ->label(__('ui.last_updated_at'))
+                                    ->icon('heroicon-o-calendar-days')
+                                    ->placeholder('-')
                                     ->dateTime(),
                             ])->columns(4),
                     ]),
